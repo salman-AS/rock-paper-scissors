158a195ac554db10828d4f8a5536cba928e1ef41
salman-AS <salmans581@gmail.com>
2024-01-13 14:49:14 +0530

    made a basic ui and a litlle design

--- javascript.js
+++ javascript.js
@@ -1,46 +1,74 @@
-function getComputerChoice(){
-    let items = ["rock","paper","scissors"];
-    let choice = items[Math.floor(Math.random()*items.length)];
+let player = 0;
+let computer = 0;
+let winner;
+
+
+const result = document.querySelector(".result");
+const playerScore = document.querySelector("#player");
+const computerScore = document.querySelector("#computer");
+const final = document.querySelector(".final");
+const btns = document.querySelectorAll(".button");
+btns.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        let playerChoice = btn.id;
+        let computerChoice = getComputerChoice();
+        console.log(playerChoice);
+        winner = playRound(playerChoice, computerChoice);
+        playGame();
+    });
+})
+
+function getComputerChoice() {
+    let items = ["rock", "paper", "scissors"];
+    let choice = items[Math.floor(Math.random() * items.length)];
     return choice;
 }
 
-function playRound(playerSelection, computerSelection){
-    if(playerSelection === computerSelection)
+function playRound(playerSelection, computerSelection) {
+
+    if (playerSelection === computerSelection) {
+        result.textContent = "It's a tie!!!";
         console.log("It's a tie!");
-    else if((playerSelection == "rock" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "rock")){
+    }
+    else if ((playerSelection == "rock" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "rock")) {
+        result.textContent = `You lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}!`;
         console.log(`You lose! ${computerSelection.toUpperCase()} beats ${playerSelection.toUpperCase()}!`);
         return 0;
     }
-    else if((playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper") || (playerSelection == "rock" && computerSelection == "scissors")){
+    else if ((playerSelection == "paper" && computerSelection == "rock") || (playerSelection == "scissors" && computerSelection == "paper") || (playerSelection == "rock" && computerSelection == "scissors")) {
+        result.textContent = `You win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}!`;
         console.log(`You win! ${playerSelection.toUpperCase()} beats ${computerSelection.toUpperCase()}!`);
         return 1;
     }
-    else{
+    else {
         console.log("Invalid choice!");
+        return 2;
     }
 }
 
-function game(){
-    let player=0;
-    let computer=0;
-    for(let i=0;i<5;i++){
-        let ps = prompt("Enter your choice").toLowerCase();
-        let cs = getComputerChoice();
-        console.log(`Player chose: ${ps}`);
-        console.log(`Computer chose: ${cs}`);
-        let rv = playRound(ps,cs);
-        if(rv==0)
-            computer++;
-        else if(rv==1)
-            player++;
-        console.log(`Points\nPlayer: ${player} | Computer: ${computer}`)
-    }
-    if(player>computer)
+
+
+function playGame() {
+    final.textContent = "";
+    if (winner == 0)
+        computer++;
+    else if (winner == 1)
+        player++;
+    playerScore.textContent = `${player}`;
+    computerScore.textContent = `${computer}`;
+    console.log(`Points\nPlayer: ${player} | Computer: ${computer}`);
+    if (player == 5){
+        player = 0;
+        computer = 0;
+        winner = 0;
+        final.textContent = "You WIN!!"
         console.log("You WIN!!");
-    else if(computer>player)
+    }
+    else if (computer == 5){
+        player = 0;
+        computer = 0;
+        winner = 0;
+        final.textContent = "You LOSE!!"
         console.log("You LOSE!!");
-    else
-        console.log("It's a TIE!!");
+    }
 }
-
-game();
